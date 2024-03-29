d333c2a5e22a7ec90901f24c43b0e68c4a5a0bbd
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-03-29 14:33:08 -0500

    Finalización de JAVASCRIPT NAVEGADOR DOM CON SCRPTING

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -59,6 +59,84 @@ function imprimir() {
   console.log(2);
 }
 
-window.onscroll = function () {
-  console.log("scrolling... ");
+/* window.onscroll = function (evento) {
+  console.log(evento);
+}; */
+
+//SELECCIONAR ELEMENTOS Y ASOCIARLES EN UN EVENTO
+/* 
+const btnEnviar = document.querySelector(".boton--primario");
+btnEnviar.addEventListener("click", function (evento) {
+  console.log(evento.target);
+  evento.preventDefault();
+
+  //validar un formulario
+
+  console.log("enviando formulario");
+}); */
+
+//EVENTOS DE LOS IMPUTS Y TEXAREA
+
+const datos = {
+  nombre: "",
+  email: "",
+  mensaje: ""
 };
+
+const nombre = document.querySelector("#nombre");
+const email = document.querySelector("#email");
+const mensaje = document.querySelector("#mensaje");
+const formulario = document.querySelector(".formulario");
+
+nombre.addEventListener("input", leerTexto);
+email.addEventListener("input", leerTexto);
+mensaje.addEventListener("input", leerTexto);
+
+//EL EVENTO DE SUBMIT
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+
+  //validar el formulario
+
+  const { nombre, email, mensaje } = datos;
+
+  if (nombre === "" || email === "" || mensaje === "") {
+    mostrarAlerta("Todos los campos son Obligatorios", true);
+    return; //corta la ejecución del código
+  }
+  mostrarAlerta("Formulario enviado Correctamente");
+
+  //Enviar formulario
+
+  console.log("enviando el formulario");
+});
+
+function leerTexto(evento) {
+  /* console.log(evento.target.value); */
+
+  datos[evento.target.id] = evento.target.value;
+  //console.log(datos);
+}
+
+//REFACTORIZANDO ALERTA
+
+function mostrarAlerta(mensaje, error = null) {
+  const alerta = document.createElement("P");
+  alerta.textContent = mensaje;
+
+  if (error) {
+    alerta.classList.add("error");
+  } else {
+    alerta.classList.add("correcto");
+  }
+     formulario.appendChild(alerta);
+     tiempoVisualizacion();
+}
+//FUNCION PARA DESAPARECER
+
+function tiempoVisualizacion() {
+   setTimeout(() => {
+    alerta.remove();
+   }, 5000);
+}
+
